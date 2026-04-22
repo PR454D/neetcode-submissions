74796b44f8f41a54937ab788eaa463400ee17bbe
Prasad Sawool <77245742+PR454D@users.noreply.github.com>
2026-04-22 12:22:11 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buy = prices[0];
+        for (int i = 0; i < prices.length; i++) {
+            int current = prices[i] - buy;
+            if (current < 0) {
+                buy = prices[i];
+            } else if (current > maxProfit) {
+                maxProfit = current;
+            }
+        }
+        return maxProfit;
+    }
+}
