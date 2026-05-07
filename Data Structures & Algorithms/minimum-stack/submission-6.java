f88e8f9ad0fba9283497304d6846f14f05ee0b75
Prasad Sawool <77245742+PR454D@users.noreply.github.com>
2026-05-07 14:31:57 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,40 @@
+class MinStack {
+    // to store differences between current min and elements
+    private final Stack<Long> stack;
+    private long min;
+
+    public MinStack() {
+        this.stack = new Stack<Long>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            this.stack.push(0L); // diff is zero
+            min = val;
+        } else {
+            this.stack.push((long) val - min);
+            if (val < min)
+                min = val;
+        }
+    }
+
+    public void pop() {
+        if (this.stack.isEmpty())
+            return;
+        long diff = this.stack.pop();
+        if (diff < 0)
+            min = min - diff;
+    }
+
+    public int top() {
+        long top = this.stack.peek();
+        if (top > 0)
+            return (int) (top + min);
+        else
+            return (int) min;
+    }
+
+    public int getMin() {
+        return (int) this.min;
+    }
+}
