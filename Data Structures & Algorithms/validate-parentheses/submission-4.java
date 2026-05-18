8caad91cfca0ea485681dac5bbf7604235fb32d1
Prasad Sawool <77245742+PR454D@users.noreply.github.com>
2026-05-18 22:27:02 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> seen = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == '{')
+                seen.push('}');
+            else if (c == '[')
+                seen.push(']');
+            else if (c == '(')
+                seen.push(')');
+            else if (seen.isEmpty() || seen.pop() != c)
+                return false;
+        }
+        return seen.isEmpty();
+    }
+}
