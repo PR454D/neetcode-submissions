04db5261cbdf449cd371c862fa5930dfde3ac2ef
Prasad Sawool <77245742+PR454D@users.noreply.github.com>
2026-07-28 12:09:00 +0530

    Add: find-the-town-judge - submission-3

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-3.py
@@ -0,0 +1,19 @@
+# check len(trust) is less than n.. i.e at least one person does not trust anybody.
+# else return -1
+
+# check if everyone trusts the same person found in step 1, remove from array if not.
+# if len of array is 1, return the item, else -1.
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        delta = defaultdict(int)
+
+        for src, dst in trust:
+            delta[src] -= 1
+            delta[dst] += 1
+        
+        for i in range(1, n + 1):
+            if delta[i] == n - 1:
+                return i
+        
+        return -1
